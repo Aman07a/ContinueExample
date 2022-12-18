95e3fb008fde47f9f415da1e13d2c6e658570b6d
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-18 10:16:37 +0100

    Continue

--- ContinueExample/Program.cs
+++ ContinueExample/Program.cs
@@ -4,7 +4,17 @@
 	{
 		static void Main(string[] args)
 		{
-			// 
+			// loop
+			for (int i = 1; i <= 10; i++)
+			{
+				if (i == 6)
+				{
+					continue;
+				}
+				System.Console.WriteLine(i);
+			}
+
+			System.Console.ReadKey();
 		}
 	}
 }
